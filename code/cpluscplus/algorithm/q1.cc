c4728e202b706bf3ccf90e9c5b8fe9183a5abe5a
Jinki Lee <jinkilee73@gmail.com>
2019-06-04 06:04:25 +0900

    add algorithm

---
+++ code/cpluscplus/algorithm/q1.cc
@@ -0,0 +1,16 @@
+// Merge Sort: Counting Inversions
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+long countInversion(vector<int> arr) {
+	return 1;
+}
+
+int main() {
+	vector<int> a{ 1,2,3,4,5 };
+	countInversion(a);
+	return 0;
+}
