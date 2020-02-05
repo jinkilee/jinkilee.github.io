dcb3718a17f6e139a7d1cc91854640c9d3d84087
Jinki Lee <jinkilee73@gmail.com>
2020-02-05 08:37:45 -0500

    add ubuntu installation script

---
+++ script/ubuntu/environment.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+# python environment
+sudo apt-get install python3-pip
+sudo pip3 install virtualenv
+# test virtualenv with the following command
+#virtualenv -p /usr/bin/python3.6 venv
